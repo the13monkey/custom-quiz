9894cad717c58803216c11d42e37e267ebf813b0
Dinah Chen <Dinah>
2019-06-28 16:56:47 -0700

    second upload

--- custom-quiz-builder.php
+++ custom-quiz-builder.php
@@ -33,43 +33,97 @@
     add_action('init', 'custom_quiz_builder');
 
     function add_scripts() {
-        $des = plugins_url('css/style.css', __FILE__);
-        wp_enqueue_style( 'style', $des, false, '','all');
+        $des_css = plugins_url('css/style.css', __FILE__);
+        $des_js = plugins_url('js/quiz.js', __FILE__);
+        wp_enqueue_style( 'style', $des_css, false, '','all');
+        wp_enqueue_script('quiz', $des_js, array('jquery'), 1.1, true);
     }
-
     add_action('init', 'add_scripts');
 
     function quiz_questions() {
+       add_meta_box('quiz_id', 'Shourtcode', 'quiz_id_metabox_display', 'quiz', 'side', 'low');
+
        add_meta_box('quiz_questions_customfields', '<h3>Custom Quiz Questions</h3>', 'quiz_questions_metabox_display', 'quiz', 'normal', 'high');
        add_meta_box('quiz_answers_customfields', '<h3>Custom Quiz Answers</h3>', 'quiz_answers_metabox_display', 'quiz', 'normal', 'high');
        add_meta_box('quiz_results_customfields', '<h3>Custom Quiz Results</h3>', 'quiz_results_metabox_display', 'quiz', 'normal', 'high');
-       
     }
-
     add_action('add_meta_boxes', 'quiz_questions');
 
+    function quiz_id_metabox_display() {
+        global $post; 
+        $output_id = $post->ID;
+        $output_title = $post->post_title;
+?>
+        <p><?php echo "[custom_quiz_".$output_id."]"; ?></p>
+<?php 
+    }
+
     function quiz_questions_metabox_display() {
-        echo "this is where the questions are.";
+        global $post; 
+        $first_question = get_post_meta($post->ID, 'question_one', true);
+        $second_question = get_post_meta($post->ID, 'question_two', true);
+        $third_question = get_post_meta($post->ID, 'question_three', true);
+        
+?>
+    <label>Question One</label>
+    <input type="text" name="question_one" placeholder="First Question" class="input_field" value="<?php print $first_question; ?>">
+    <label>Question Two</label>
+    <input type="text" name="question_two" placeholder="Second Question" class="input_field" value="<?php print $second_question ?>">
+    <label>Question Three</label>
+    <input type="text" name="question_three" placeholder="Third Question" class="input_field" value="<?php print $third_question ?>">
+<?php
     }
 
     function quiz_answers_metabox_display() {
-        echo "this is where the answers are.";
+?>
+    <label>Answers to Question One</label>
+<?php 
     }
 
     function quiz_results_metabox_display() {
         echo "this is where the results are.";
     }
 
+    function quiz_metabox_save($post_id) {
+        $is_autosave = wp_is_post_autosave($post_id);
+        $is_revision = wp_is_post_revision($post_id);
+        
+        if($is_autosave || $is_revision) {
+            return; 
+        }
 
-    function output_custom_quiz() { 
-        $content = '';
-        $content .= '<div id="quiz-body">
-                        <div id="quiz-wrapper">
-                            <h1>This is where the quiz goes.</h1>
-                        </div>
-                    </div>';
+        $post = get_post($post_id); 
 
-        return $content; 
+        if($post->post_type == "quiz") {
+           
+            if(array_key_exists('question_one', $_POST)) {
+                update_post_meta($post_id, 'question_one', $_POST['question_one']);
+            }
+            if(array_key_exists('question_two', $_POST)) {
+                update_post_meta($post_id, 'question_two', $_POST['question_two']);
+            }
+            if(array_key_exists('question_three', $_POST)) {
+                update_post_meta($post_id, 'question_three', $_POST['question_three']);
+            }
+        }
+    }
+    add_action('save_post', 'quiz_metabox_save');
+
+
+    function output_custom_quiz($output_id) { 
+        $output_id = 250; 
+        $post = get_post($output_id);
+        $quiz_heading = $post->post_title;
+?>
+    <div id="quiz-body">
+        <div id="quiz-wrapper">
+            <div id="quiz-title">
+                <h1><?php print $quiz_heading ?></h1>
+            </div>
+        </div>
+    </div>
+<?php
+        
         /*
         $args = array (
             'posts_per_page' => -1,
@@ -88,5 +142,5 @@
         */
         
     }
-    add_shortcode('custom_quiz', 'output_custom_quiz');
+    add_shortcode('custom_quiz_250', 'output_custom_quiz');
 ?>
